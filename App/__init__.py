e1179b990f1eca14745a974ec647dc3504ceada0
Alexander Bastidas <xanderjorge@hotmail.com>
2024-10-01 13:35:55 -0500

    archivo principal

--- App/__init__.py
+++ App/__init__.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def Index():
-    return '<h1> Hello  </h1>'
+    return render_template('base.html')
 
 if __name__ == '__main__':
     app.run(debug=True,port=8000)
